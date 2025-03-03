d01e2ccab4a7f0f2582686db967de4616bfe0116
Shubham Rai <shubhamrai01683@gmail.com>
2025-03-03 17:39:43 +0530

    Final changes

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,15 +1,22 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+public class BankAccount {
+    private double balance;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    public BankAccount(double initialBalance){
+        if (initialBalance >= 0) {
+            this.balance = initialBalance;
+            System.out.println("Created the Bank Account with Initial Balance : "+initialBalance);
+        } else {
+            System.out.println("Error: Initial balance cannot be negative. Setting to $0.");
+            this.balance = 0;
         }
+    }
+    public BankAccount(){
+        this(0);
+    }
+
+public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(500);
+        BankAccount account2 = new BankAccount();
+    BankAccount account3 = new BankAccount();
     }
 }
